c61bcdb523a23600eda18c7472ddca4cfcdd49e7
stef.lauwaet <stef.lauwaet@student.howest.be>
2021-01-19 20:50:13 +0100

    Finish tutorial

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -20,14 +20,18 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
-    
+    @app.route("/hello")
+    def hello():
+       return "Hello, World!"
+
     from . import db
     db.init_app(app)
     
     from . import auth
     app.register_blueprint(auth.bp)
-    
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
